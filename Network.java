ccfb94f4b0ee03ef40af196f6df5f8a130eb682c
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2026-01-04 13:46:35 +0200

    hw done

--- Network.java
+++ Network.java
@@ -110,10 +110,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network:";
+       String ans = "Network:\n";
        for (int i = 0; i < userCount; i++) {
-           ans += "\n" + users[i].toString();
-           if(userCount > 0) ans += "\n";
+           ans += users[i].toString() + "\n";
        }
        return ans;
     }
